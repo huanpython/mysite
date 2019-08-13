8aa0e98fb41637bc417124270f81e7d8d7269970
huanfuan <huanfuan@163.com>
2019-08-13 12:30:10 +0800

    add 添加要显示的字段

--- trade/admin.py
+++ trade/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from trade.models import ShoppingCart, OrderInfo, OrderGoods
 
-admin.site.register(ShoppingCart)
-admin.site.register(OrderInfo)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ["user", "goods", "nums", ]
+
+
+class OrderInfoAdmin(admin.ModelAdmin):
+    list_display = ["user", "order_sn",  "trade_no", "pay_status", "post_script", "order_mount",
+                    "order_mount", "pay_time", "add_time"]
+
+
+
+
+
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(OrderInfo, OrderInfoAdmin)
 admin.site.register(OrderGoods)
--- user_operation/admin.py
+++ user_operation/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from user_operation.models import UserFav,UserLeavingMessage, UserAddress
 
-admin.site.register(UserFav)
-admin.site.register(UserLeavingMessage)
-admin.site.register(UserAddress)
+
+
+class UserFavAdmin(admin.ModelAdmin):
+    list_display = ['user', 'goods', "add_time"]
+
+
+class UserLeavingMessageAdmin(admin.ModelAdmin):
+    list_display = ['user', 'message_type', "message", "add_time"]
+
+
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display = ["signer_name", "signer_mobile", "district", "address"]
+
+
+
+
+
+admin.site.register(UserFav, UserFavAdmin)
+admin.site.register(UserLeavingMessage, UserLeavingMessageAdmin)
+admin.site.register(UserAddress, UserAddressAdmin)
